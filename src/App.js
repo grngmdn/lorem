a274a462780050ae0bf7dc4b79cfe135b110152c
Madan gurung <gurungmadan@hotmail.com>
2022-03-03 03:55:34 +0000

    creating a layout heading and a form input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
+import data from "./Data"
 
 function App() {
+
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState([])
+
+
+
   return (
     <div>
         <h1>
-          app
+          Lorem Ipsum Generator
         </h1>
+        <form onSubmit={handleSubmit}>
+          <span>Paragraphs: </span>
+          <input type="number" value={count} onChange={(e) => { setCount(e.target.value) }} />
+          <button type="submit">GENERATE</button>
+        </form>
+
     </div>
   );
 }
