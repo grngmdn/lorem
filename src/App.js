5b69e037c2406fc8a4beaf83c3f22cc334f3dade
Madan gurung <gurungmadan@hotmail.com>
2022-03-03 04:17:10 +0000

    adding logic to display appropirate number of paragraph

--- src/App.js
+++ src/App.js
@@ -9,8 +9,19 @@ function App() {
   const  handleSubmit = (e) => {
     e.preventDefault();
 
-    setText(data)
-    console.log(text)
+    // changes the string to number 
+    let amount = parseInt(count)
+
+    // making sure the page displays at least 1 paragraph of lorem ipsum and no more than the maximum of 8 paragraph as there are only 8 arrays in the data  
+    if(count <= 0){
+      amount = 1
+    }
+    if(count > 8){
+      amount = 8
+    }
+
+    // setting the state of text to an array of given numbers of data
+    setText(data.slice(0, amount))
   }
 
   return (
@@ -20,15 +31,17 @@ function App() {
         </h1>
         <form onSubmit={handleSubmit}>
           <span>Paragraphs: </span>
+          {/* this sets the state of count to whatever is typed in to the number input */}
           <input type="number" value={count} onChange={(e) => { setCount(e.target.value) }} />
           <button type="submit">GENERATE</button>
         </form>
+        {/* looping over the array of data and displaying it on the page  */}
         <div>{text.map((item, index) => {
-
           return(
             <p key={index}>{item}</p>
           )
-        })}</div>
+        })}
+        </div>
     </div>
   );
 }
